cfd3a6d893686370bdb69ea2d06e936660bd641e
Tushar <tgawande04@gmail.com>
2023-10-09 17:06:21 +0530

    Code Commited

--- LearnCollection/StartCode.java
+++ LearnCollection/StartCode.java
@@ -14,17 +14,31 @@ public class StartCode {
 
          //TypeSafe Collection
          ArrayList<String> names = new ArrayList<>();
+         //add
          names.add("Tushar");
          names.add("Gawande");
          names.add("Patil");
+         names.add("Gawande");
          
          System.out.println(names);
-         System.out.println(names.get(0));
-         System.out.println(names.get(1));
+        //  System.out.println(names.get(0));
+        //  System.out.println(names.get(1));
 
-         //UnTypeSafe Collection
-         LinkedList list = new LinkedList();
-         
+        //  //UnTypeSafe Collection
+        //  LinkedList list = new LinkedList();
+        //  list.add("Sachin");
+        //  list.add(100);
+        //  list.add(302.665);
+        //  list.add(true);
+
+        //  System.out.println(list);
+       
+        //delete
+        names.remove("Gawande");
+        //size
+        System.out.println("Size = "+names.size());
+        //check item is present there or not
+        System.out.println(names.contains("Patil"));         
     }
     
 }
